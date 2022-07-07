6e8bb7131162308b22ba4ded0cca475e21522da0
Tuba <gulertubakemanci@gmail.com>
2022-07-07 17:53:40 +0100

    HectorWare

---
+++ src/test/java/com/HectorWare/step_definitions/login_Guler_StepDef.java
@@ -0,0 +1,21 @@
+package com.HectorWare.step_definitions;
+
+import io.cucumber.java.en.Given;
+import io.cucumber.java.en.Then;
+import io.cucumber.java.en.When;
+
+public class login_Guler_StepDef {
+
+    @Given ("the user navigates to the url")
+        public void the_user_navigates_to_url(){
+
+        }
+   @When ("the user enters username and password")
+    public void the_user_enters_username_and_password(){
+
+   }
+   @ Then ("the user should see the main page")
+    public void the_user_should_see_the_main_page(){
+
+   }
+}
--- src/test/java/com/HectorWare/utilities/Web_Utils.java
+++ src/test/java/com/HectorWare/utilities/Web_Utils.java
@@ -1,8 +1,9 @@
 package com.HectorWare.utilities;
 
-import org.openqa.selenium.By;
-import org.openqa.selenium.WebDriverException;
-import org.openqa.selenium.WebElement;
+import org.junit.Assert;
+import org.openqa.selenium.*;
+import org.openqa.selenium.interactions.Actions;
+import org.openqa.selenium.support.ui.ExpectedCondition;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.Select;
 import org.openqa.selenium.support.ui.WebDriverWait;
